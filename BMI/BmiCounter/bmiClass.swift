2f9b527d86feceacab5fd76bb52ebb9216854035
Piotr Kupczyk <piotr.kupczyk97@gmail.com>
2018-06-21 22:01:34 +0200

    edited all values as Measurment

--- BMI/BmiCounter/bmiClass.swift
+++ BMI/BmiCounter/bmiClass.swift
@@ -12,7 +12,6 @@ class BmiCouter {
         case invalidFormat
     }
     
-    
     private func checkValue(value: Double, range: ClosedRange<Double>) throws -> Double {
         if range.contains(value) {
             return value
@@ -22,117 +21,36 @@ class BmiCouter {
     }
     
     init(centimeters: Double, kilograms: Double) {
-        self.centimeters = centimeters
-        self.kilograms = kilograms
+        self.centimeters = Measurement(value: centimeters, unit: UnitLength.centimeters)
+        self.kilograms = Measurement(value: kilograms, unit: UnitMass.kilograms)
+        self.pounds = self.kilograms.converted(to: UnitMass.pounds)
+        let tempFeet = self.centimeters.converted(to: UnitLength.feet)
+        let inchAsPartOfFeet = Measurement(value: tempFeet.value.truncatingRemainder(dividingBy: 1), unit: UnitLength.feet)
+        self.feet = Measurement(value: floor(tempFeet.value), unit: UnitLength.feet)
+        self.inches = Measurement(value: round(inchAsPartOfFeet.value), unit: UnitLength.inches)
     }
     
     init(feet: Double, inches: Double, pounds: Double) {
-        self.feet = feet
-        self.inches = inches
-        self.pounds = pounds
+        self.feet = Measurement(value: feet, unit: UnitLength.feet)
+        self.inches = Measurement(value: inches, unit: UnitLength.inches)
+        self.pounds = Measurement(value: feet, unit: UnitMass.pounds)
+        self.centimeters = self.feet.converted(to: UnitLength.centimeters)
+                            + self.inches.converted(to: UnitLength.centimeters)
+        self.kilograms = self.pounds.converted(to: UnitMass.kilograms)
     }
     
     func cout() -> Double {
-        return kilograms/(pow(centimeters/100, 2))
+        return kilograms.value/(pow(centimeters.converted(to: UnitLength.meters).value, 2))
     }
-    
-    func metricToImperial(kg: Double, cm: Double) {
-        
-    }
-    
-    private struct Pair<T> {
-        let first: T
-        let second: T
-        init(first: T, second: T) {
-            self.first = first
-            self.second = second
-        }
-    }
-
-    
 
     //------------------------------Values getters and setters---------------------
     
     //metric units
-    private var centimeters: Double {
-        set(value) {
-            let centimetersRange = 60.96...243.84
-            do {
-                self.inches = try checkValue(value: value, range: centimetersRange)
-            } catch BmiErrors.invalidFormat {
-                print("Error")
-            } catch {
-                print("any other")
-            }
-        }
-        get {
-            return self.centimeters
-        }
-        
-    }
-    private var kilograms: Double {
-        
-        set(value) {
-            let kilogramsRange = 24.95...181.44
-            do {
-                self.inches = try checkValue(value: value, range: kilogramsRange)
-            } catch BmiErrors.invalidFormat {
-                print("Error")
-            } catch {
-                print("any other")
-            }
-        }
-        get {
-            return self.kilograms
-        }
-        
-    }
+    private var centimeters: Measurement<Dimension>
+    private var kilograms: Measurement<Dimension>
     
     //imperial units
-    var feet: Double {
-        set(value) {
-            let feetRange = 2.0...7.0
-            do {
-                self.inches = try checkValue(value: value, range: feetRange)
-            } catch BmiErrors.invalidFormat {
-                print("Error")
-            } catch {
-                print("any other")
-            }
-        }
-        get {
-            return self.feet
-        }
-    }
-    
-    var inches: Double {
-        set(value) {
-            let inchesRange = 0.0...12.0
-            do {
-                self.inches = try checkValue(value: value, range: inchesRange)
-            } catch BmiErrors.invalidFormat {
-                print("Error")
-            } catch {
-                print("any other")
-            }
-        }
-        get {
-            return self.inches
-        }
-    }
-    var pounds: Double {
-        set(value) {
-            let inchesRange = 55.0...400.0
-            do {
-                self.pounds = try checkValue(value: pounds, range: 55.0...400.0)
-            } catch BmiErrors.invalidFormat {
-                print("Error")
-            } catch {
-                print("any other")
-            }
-        }
-        get {
-            return self.inches
-        }
-    }
+    var feet: Measurement<Dimension>
+    var inches: Measurement<Dimension>
+    var pounds: Measurement<Dimension>
     }
